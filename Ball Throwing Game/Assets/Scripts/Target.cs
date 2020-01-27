d5bb9ffedfa46edb3c136678ec3425a356247375
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-01-26 23:55:14 -0600

    First Commit

---
+++ Ball Throwing Game/Assets/Scripts/Target.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Target : MonoBehaviour
+{
+    public Score scoreManager;
+    
+    private void OnCollisionEnter(Collision collision) {
+
+        switch(collision.gameObject.tag){
+            case "Friendly":
+                // print("Collision Plane");
+                break;
+            case "Player":
+                // print("Player collided. destroy me");
+                Destroy(gameObject);
+                scoreManager.AddPoint();
+                break;
+            default:
+                print("something wrong");
+                break;
+        } 
+        // Destroy(gameObject);
+    }
+}
